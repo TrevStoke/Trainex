f67f8ea3f5aea92b36d41a1dd31624700434c271
Andrew Cornes <andrew@andrewcornes.com>
2013-09-16 14:34:44 +0100

    Initial versions of the 'Employees' model and associated test files.

---
+++ app/Model/Employee.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Employee Model
+ *
+ */
+class Employee extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+
+}
---
+++ app/Test/Fixture/EmployeeFixture.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * EmployeeFixture
+ *
+ */
+class EmployeeFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'key' => 'unique', 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'active' => array('type' => 'integer', 'null' => false, 'default' => '1'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'name' => array('column' => 'name', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'name' => 'Lorem ipsum dolor sit amet',
+			'active' => 1,
+			'created' => '2013-09-16 14:28:05',
+			'modified' => '2013-09-16 14:28:05'
+		),
+	);
+
+}
