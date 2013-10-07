40615b29bcb122b22c7bd17fe395c04fb1abbd3b
Andrew Cornes <andrew@andrewcornes.com>
2013-10-07 11:45:13 +0100

    Tweaked implementation of 'isUniqueComposite()' method in 'UniqueCompositeBehavior'.

--- app/Model/Behavior/UniqueCompositeBehavior.php
+++ app/Model/Behavior/UniqueCompositeBehavior.php
@@ -3,80 +3,47 @@ App::uses('ModelBehavior', 'Model');
 
 class UniqueCompositeBehavior extends ModelBehavior
 {
+	// ------------------------------------------------------------------------
 	// '$columnNames': an array of the names of the columns which are
-	// composite-unique.
-	// To cause this method only to perform the validity check once for each
-	// set of composite-unique columns, if the method finds that there's a
-	// column with a specified value earlier in the column name list than the
-	// current column, it will return 'TRUE'.
+	// unique-composite.
 	public function isUniqueComposite(Model $model,
 		$validateField, $columnNames)
 	{
-		// Sort column names to make sure that all calls to this method use the
-		// column name list in the same order (as part of preventing multiple
-		// validity checks for a single group of composite-unique columns).
-		sort($columnNames);
-
 		$modelDataFields = $model->data[$model->alias];
-
-		reset($validateField);
-		$thisColumnName = key($validateField);
-		$firstSpecifiedColumn = TRUE;
 		$newCompositeValues = array();
 
-Debugger::dump("Validation of: $thisColumnName");
-Debugger::dump($modelDataFields);
 		// Set up an array of all the specified values for the unique-composite
 		// columns. For an insert, this should be all of them; for an update,
-		// this may not be all.
+		// it may not be all.
 		foreach ($columnNames as $columnName)
 		{
 			if (isset($modelDataFields[$columnName]) == TRUE)
 			{
-Debugger::dump("- $columnName");
-				// If the first composite unique column with a supplied value
-				// is not the column that caused this validity check to be
-				// called, then presumably the validity check has already been
-				// performed, so we can stop here.
-				if ($firstSpecifiedColumn == TRUE &&
-					$columnName != $thisColumnName)
-				{
-Debugger::dump("Validation already performed: $columnName");
-					return TRUE;
-				}
-
 				$newCompositeValues[$columnName] =
 					$modelDataFields[$columnName];
-				$firstSpecifiedColumn = FALSE;
 			}
 		}
-Debugger::dump("Validation being performed");
 
-		// If the validation is taking place due to an update, and not all of
-		// the unique-composite columns have specified values.
+		// If the validation is taking place due to an update, and if not all
+		// of the unique-composite columns have specified values, fetch current
+		// values for the row to be updated from the DB to fill in the blanks.
 		if (isset($modelDataFields[$model->primaryKey]) == TRUE &&
 			count($newCompositeValues) != count($columnNames))
 		{
-			// Fetch current values for unique-composite columns.
 			$currentFieldValues = $model->find('first', array(
 				'fields' => $columnNames,
 				'conditions' => array(
-					'id' => $modelDataFields[$model->primaryKey],
+					$model->primaryKey => $modelDataFields[$model->primaryKey],
 				),
-				'recursive' => -1,
 			));
 
-			// If not all unique-composite columns have been specified AND
-			// there's no row to update in the DB, validation fails.
 			if (count($currentFieldValues) == 0)
 			{
 				return FALSE;
 			}
 
-			// Fill in any unspecified unique-composite column values from
-			// the current-values row we just found.
 			foreach ($currentFieldValues[$model->alias] as
-					 $columnName => $columnValue)
+				$columnName => $columnValue)
 			{
 				if (isset($newCompositeValues[$columnName]) == FALSE)
 				{
@@ -86,17 +53,18 @@ Debugger::dump("Validation being performed");
 		}
 
 		// If we don't have values for all unique-composite columns by now,
-		// something is wrong, e.g., not all unique-composites given a value
-		// as part of an insert (which really should be picked up by the
+		// something is wrong, e.g., not all unique-composites were given a
+		// value as part of an insert (which really should be picked up by the
 		// "'required' => 'create'" validation for those columns).
 		if (count($newCompositeValues) != count($columnNames))
 		{
 			return FALSE;
 		}
 
-		// If we find anything, validation should fail.
+		// If we find a match using the supplied (and perhaps current) values,
+		// validation should fail.
 		return $model->find('count', array(
 			'conditions' => $newCompositeValues,
-		)) === 0;
+		)) == 0;
 	}
 }
